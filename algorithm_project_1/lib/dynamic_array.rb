6d3b925cb63e9da37790638a379665a9fafcde9b
hyunckim <hyunkim1209@gmail.com>
2017-04-11 21:06:30 -0700

    Add static and dynamic array

--- algorithm_project_1/lib/dynamic_array.rb
+++ algorithm_project_1/lib/dynamic_array.rb
@@ -4,31 +4,61 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @capacity = 8
+    @length = 0
+    @store = StaticArray.new(@capacity)
+    @start_idx = 0
   end
 
   # O(1)
   def [](index)
+    raise 'index out of bounds' if index >= @length
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    raise 'index out of bounds' if @length == 0
+    @store[@length] = nil
+    @length -=1
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    if @store.length == @length
+      resize!
+    end
+    @store[@length] = val
+    @length+=1
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise 'index out of bounds' if @length == 0
+    @length -= 1
+    shift = @store[0]
+    (0..(@length-1)).each do |i|
+      @store[i] = @store[i + 1]
+    end
+    shift
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    @length +=1
+    if @length == @capacity
+      resize!
+    end
+    @length.downto(1) do |i|
+      @store[i] = @store[i-1]
+    end
+    @store[0] = val
   end
 
   protected
@@ -40,5 +70,11 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    temp_store = @store
+    @capacity += 8
+    @store = StaticArray.new(@capacity)
+    (0..@length).each do |i|
+      @store[i] = temp_store[i]
+    end
   end
 end
--- algorithm_project_1/lib/static_array.rb
+++ algorithm_project_1/lib/static_array.rb
@@ -1,14 +1,22 @@
 # This class just dumbs down a regular Array to be staticly sized.
 class StaticArray
+
+  attr_reader :length
+
   def initialize(length)
+    @length = length
+    @store = Array.new(length)
+
   end
 
   # O(1)
   def [](index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    @store[index] = value
   end
 
   protected
