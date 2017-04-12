988c3c49d29ec4303b65a17d614248155c7fbd56
hyunckim <hyunkim1209@gmail.com>
2017-04-11 22:15:03 -0700

    complete ring buffer array

--- algorithm_project_1/lib/ring_buffer.rb
+++ algorithm_project_1/lib/ring_buffer.rb
@@ -4,30 +4,55 @@ class RingBuffer
   attr_reader :length
 
   def initialize
+    @capacity = 8
+    @start_idx = 0
+    @store = StaticArray.new(@capacity)
+    @length = 0
   end
 
   # O(1)
   def [](index)
+    raise 'index out of bounds' if index >= @length
+    @store[index + @start_idx]
   end
 
   # O(1)
   def []=(index, val)
+    @store[index + @start_idx] = val
   end
 
   # O(1)
   def pop
+    raise 'index out of bounds' if @length == 0
+    pop = @store[@length-1 + @start_idx]
+    @store[@length - 1 + @start_idx] = nil
+    @length -=1
+    pop
   end
 
   # O(1) ammortized
   def push(val)
+    @length+=1
+    resize! if @length > @capacity
+    @store[@length-1 + @start_idx] = val
   end
 
   # O(1)
   def shift
+    raise 'index out of bounds' if @length == 0
+    @length -= 1
+    shift = @store[@start_idx]
+    @store[@start_idx] = nil
+    @start_idx += 1
+    shift
   end
 
   # O(1) ammortized
   def unshift(val)
+    @length += 1
+    resize! if @length > @capacity
+    @start_idx -= 1
+    @store[@start_idx] = val
   end
 
   protected
@@ -38,5 +63,13 @@ class RingBuffer
   end
 
   def resize!
+    temp_store = @store
+    @capacity += 8
+    @store = StaticArray.new(@capacity)
+    (@length-1).times do |i|
+      @store[i] = temp_store[@start_idx + i]
+    end
+    p temp_store
+    @start_idx = 0
   end
 end
