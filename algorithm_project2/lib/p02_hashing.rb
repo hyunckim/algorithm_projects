13b409e2ed441a2be69fbce28004ba67d10948ab
hyunckim <hyunkim1209@gmail.com>
2017-04-12 19:48:06 -0700

    Complete hash for other data types

--- algorithm_project2/lib/p02_hashing.rb
+++ algorithm_project2/lib/p02_hashing.rb
@@ -4,11 +4,16 @@ end
 
 class Array
   def hash
+    return 0.hash if self.empty?
+    hash = self.map.with_index { |el, index| (el*(index+1)).hash }
+    hash.inject(:+)
   end
 end
 
 class String
   def hash
+    hash = self.chars.map.with_index { |char| char.ord }
+    hash.hash
   end
 end
 
@@ -16,6 +21,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash = 0
+    self.each do |key, value|
+      hash += key.hash + value.hash
+    end
+    hash
   end
 end
