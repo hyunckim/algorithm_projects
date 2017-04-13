726c7bf6f710543aff34ffd2df720d087450b8a6
hyunckim <hyunkim1209@gmail.com>
2017-04-12 19:15:43 -0700

    Complete Set exercises

--- algorithm_project2/lib/p01_int_set.rb
+++ algorithm_project2/lib/p01_int_set.rb
@@ -1,22 +1,31 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max) { false }
   end
 
   def insert(num)
+    is_valid?(num)
+    @store[num-1] = true
   end
 
   def remove(num)
+    is_valid?(num)
+    @store[num-1] = false
   end
 
   def include?(num)
+    @store[num-1]
   end
 
   private
 
   def is_valid?(num)
+    raise 'Out of bounds' if num >= @max || num < 0
   end
 
   def validate!(num)
+
   end
 end
 
@@ -27,12 +36,15 @@ class IntSet
   end
 
   def insert(num)
+    @store[num % num_buckets].push(num)
   end
 
   def remove(num)
+    @store[num % num_buckets].delete(num)
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
@@ -55,12 +67,18 @@ class ResizingIntSet
   end
 
   def insert(num)
+    @count += 1
+    resize! if @count > num_buckets
+    @store[num % num_buckets].push(num)
   end
 
   def remove(num)
+    @store[num % num_buckets].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
@@ -74,5 +92,12 @@ class ResizingIntSet
   end
 
   def resize!
+    temp_array = @store
+    @store = Array.new(num_buckets*2) { Array.new }
+    temp_array.each do |bucket|
+      bucket.each do |el|
+        @store[el % num_buckets].push(el)
+      end
+    end
   end
 end
