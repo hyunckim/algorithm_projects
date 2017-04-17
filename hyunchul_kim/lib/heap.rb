1ee42991fcc60847595df662fa675e3ec63cb471
hyunckim <hyunkim1209@gmail.com>
2017-04-16 21:27:25 -0700

    Add methods to find child and parent indices

--- hyunchul_kim/lib/heap.rb
+++ hyunchul_kim/lib/heap.rb
@@ -1,17 +1,23 @@
 class BinaryMinHeap
   def initialize(&prc)
+    @store = []
+    @prc = prc
   end
 
   def count
+
   end
 
   def extract
+
   end
 
   def peek
+
   end
 
   def push(val)
+
   end
 
   protected
@@ -19,9 +25,20 @@ class BinaryMinHeap
 
   public
   def self.child_indices(len, parent_index)
+    left_child = (parent_index * 2) + 1
+    right_child = (parent_index * 2) + 2
+    if left_child >= len
+      return []
+    elsif right_child >= len
+      return [left_child]
+    else
+      return [left_child, right_child]
+    end
   end
 
   def self.parent_index(child_index)
+    raise 'root has no parent' if child_index == 0
+    parent_index = (child_index -1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
