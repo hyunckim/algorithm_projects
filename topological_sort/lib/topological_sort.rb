7b290f62f2596d23afe8076b8dc74f1213aa8468
hyunckim <hyunkim1209@gmail.com>
2017-04-20 11:46:07 -0700

    add implementation of topological sort using Kahns algorithm

--- topological_sort/lib/topological_sort.rb
+++ topological_sort/lib/topological_sort.rb
@@ -2,6 +2,28 @@ require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
+# Kahn's Algorithm
 def topological_sort(vertices)
+  sorted = []
+  queue = []
+  in_edge_counts = {}
 
+  vertices.each do |vertex|
+    in_edge_counts[vertex] = vertex.in_edges.count
+    queue << vertex if vertex.in_edges.empty?
+  end
+
+  until queue.empty?
+    current = queue.shift
+    sorted << current
+
+    current.out_edges.each do |edge|
+      destination = edge.to_vertex
+      in_edge_counts[destination] -= 1
+      queue << destination if in_edge_counts[destination] == 0
+    end
+  end
+  sorted
 end
+
+# Tarjan's Algorithm
